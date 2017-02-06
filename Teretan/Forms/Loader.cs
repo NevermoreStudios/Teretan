b9d8fa501ccc92e77e018e66a559466d1e85187f
lazar2222 <lazar2premovic@gmail.com>
2017-02-06 19:15:54 +0100

    initial

---
+++ Teretan/Forms/Loader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teretan
+{
+    public partial class Loader : Form
+    {
+        public Loader()
+        {
+            InitializeComponent();
+        }
+
+        private void Loader_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
